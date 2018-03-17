c7ae16dbc7021cf67aad87585d6ee61bc002bfa5
techniponi <wincam97@gmail.com>
2018-03-17 05:35:21 -0500

    Major code restructure to utilize webpack

---
+++ cameronseid.js
@@ -0,0 +1,20 @@
+// load css
+import './css/icon.css';
+import 'materialize-css/dist/css/materialize.css';
+import './css/style.css';
+import 'materialize-css/dist/js/materialize.js';
+
+$(document).ready(function () {
+
+	// load common page elements
+	$('#csheader').load('header.html');
+	$('#csfooter').load('footer.html');
+
+	// init modals
+	$('#materializelicense').modal();
+	$('#jquerylicense').modal();
+
+	// init.js
+	$('.parallax').parallax();
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './cameronseid.js',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				// Match woff2 in addition to patterns like .woff?v=1.1.1.
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [{
+					loader: "url-loader",
+					options: {
+						// Limit at 50k. Above that it emits separate files
+						limit: 50000,
+
+						// url-loader sets mimetype if it's passed.
+						// Without this it derives it from the file extension
+						mimetype: "application/font-woff",
+
+						// Output below fonts directory
+						name: "./fonts/[name].[ext]",
+					}
+				}],
+			},
+    	]
+	},
+	externals: {
+		jquery: 'jQuery'
+	},
+	plugins: [
+    new webpack.ProvidePlugin({
+			"Hammer": "hammerjs/hammer"
+		})
+  ],
+	resolve: {
+		extensions: ['.js'],
+		alias: {}
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, "build")
+	}
+};
